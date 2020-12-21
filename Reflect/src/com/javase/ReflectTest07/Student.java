e5e632c3473a3430fd2aa90c55dd45aedc12aadb
bijian0530 <1076820693@qq.com>
2020-12-21 18:13:37 +0800

    测试

---
+++ Reflect/src/com/javase/ReflectTest07/Student.java
@@ -0,0 +1,9 @@
+package com.javase.ReflectTest07;
+
+public class Student {
+    private String name;
+    protected int age;
+    boolean sex;
+    public int no=111;
+    public static final double MATH_PI = 3.1456987;
+}
